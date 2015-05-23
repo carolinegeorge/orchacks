631e9a2d6be338e482bd42bd7189afd299d72e42
haftapparat <ahnesther0702@gmail.com>
2015-05-23 17:26:12 -0400

    added some new fields to the dictionary , also wrote if statements for stuff

--- survey/music.js
+++ survey/music.js
@@ -1,29 +1,67 @@
 var dict = [{name: "Four Seasons",
-composer: "Antonio Vivaldi's",
-era: "Baroque",
-mood: "Angry"},
+composer: "Antonio Vivaldi's", 
+era: "Baroque"
+mood: "Happy"},
 {name: "5th Symphony",
-composer: "Beethoven"
+composer: "Ludwig van Beethoven",
 era: "Classical",
 mood: "Assertive"},
 {name: "Symphony in D major",
 composer: "Johann Christian Bach",
 era: "Baroque",
-<<<<<<< HEAD
-mood: "Sad"},
-{name: "Eine kleine nachmusik",
-composer: "Wolgang Amadeus Mozart 
-mood: "Sad"}}
+mood: "Sad"}]
 
 var userInputQ1 = [];
 var userInputQ2 = [];
 var userInputQ3 = [];
 var userInputQ4 = [];
+var userInputQ5 = [];
 var results = [];
 
 var inputCounter = 0;
 var resultCounter = 0;
 
+
+//if the user chooses baroque, set the input as Baroque, etc
+if(bar) { 
+    userInputQ1[inputCounter] = "Baroque";
+    inputCounter++;
+}
+
+if(clas) {
+    userInputQ1[inputCounter] = "Classical";
+    inputCounter++;
+}
+
+if(rom) {
+    userInputQ1[inputCounter] = "Romantic";
+    inputCounter++;
+}
+
+if(imp) {
+    userInputQ1[inputCounter] = "Impressionist";
+    inputCounter++;
+}
+
+if(twnc) {
+    userInputQ1[inputCounter] = "20th Century";
+    inputCounter++;
+}
+
+if(snd) {
+    userInputQ1[inputCounter] = "Soundtrack";
+    inputCounter++;
+}
+
+if(ane) {
+    userInputQ1[inputCounter] = "Any";
+    inputCounter++;
+}
+
+
+    
+
+// goes through q1 and looks for era
 for(var i = 0; i < userInputQ1.length; i++){
   for(var q = 0; q < dict.length; q++){
     if(dict[q].era === userInputQ1[i]){
@@ -32,22 +70,38 @@ for(var i = 0; i < userInputQ1.length; i++){
     }
   }
 }
+
+// goes through q2 and looks for moods
 for(var i = 0; i < userInputQ2.length; i++){
   for(var q = 0; q < dict.length; q++){
-    if(dict[q].era === userInputQ2[i]){
+    if(dict[q].mood === userInputQ2[i]){
       results[resultCounter] = dict[q];
       resultCounter++;
     }
   }
 }
+
+// goes through q3 and looks for answer (?)
 for(var i = 0; i < userInputQ3.length; i++){
   for(var q = 0; q < dict.length; q++){
-    if(dict[q].era === userInputQ3[i]){
+    if(userInputQ3[i]){
       results[resultCounter] = dict[q];
       resultCounter++;
     }
   }
 }
+
+// goes through q4 and finds Y/N/No pref
+for(var i = 0; i < userInputQ4.length; i++){
+  for(var q = 0; q < dict.length; q++){
+    if(userInputQ4[i] == "Yes"){
+      results[resultCounter] = dict[q];
+      resultCounter++;
+    }
+  }
+}
+
+// goes through q5 and fnds Y/N/No pref
 for(var i = 0; i < userInputQ4.length; i++){
   for(var q = 0; q < dict.length; q++){
     if(dict[q].era === userInputQ4[i]){

a00bfb8c62730a52d0a6755b74bf74c4e0d027df
thadeaus <graham.cooper33@gmail.com>
2015-05-23 14:55:41 -0400

    creating new file structure

---
+++ survey/music.js
@@ -0,0 +1,18 @@
+var dict = [{name: "Four Seasons",
+composer: "Antonio Vivaldi's"},
+{name: "5th Symphony",
+composer: "Beethoven"
+era: "Classical",
+mood: "Assertive"},
+{name: "Symphony in D major",
+composer: "Johann Christian Bach",
+era: "Buroque",
+mood: "Sad"},
+
+var userInput = [];
+var results = [];
+
+var inputCounter = 0;
+var resultCounter = 0;
+
+

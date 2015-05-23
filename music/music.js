579d7fb2a9df63872543a215e552d2d22d0442a7
haftapparat <ahnesther0702@gmail.com>
2015-05-23 11:36:57 -0400

    ???
    ???

--- music/music.js
+++ music/music.js
@@ -1,3 +1,5 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -31,6 +33,12 @@ if (Meteor.isClient) {
         }
       }
     }
+  });
+  
+  Template.body.helpers ({
+     tasks: function () { 
+        return Tasks.find({});
+     }
   });
 }
 

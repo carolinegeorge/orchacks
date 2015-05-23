56e156d5ca6fd47cbc98a3a616ea5e846b045328
thadeaus <graham.cooper33@gmail.com>
2015-05-23 11:36:26 -0400

    added results stuff

--- music/music.js
+++ music/music.js
@@ -1,17 +1,35 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  var dict = [{name: "New World Symphony", composer: "Antonin Dvorak", period: "Romantic", date: "2015"},
+	             {name: "Symphony No. 25 in G Minor", composer: "Wolfgang Amadeus Mozart", period: "Classical", date: "2016"}];
+  var results = [];
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+    
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  /*function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i]. === nameKey) {
+            return myArray[i];
+        }
+    }
+}*/
+  Template.resultSubmit.events({
+    "submit": function() {
+      var date = "2015";
+      var period = "Romantic";
+      var counter = 0;
+      for(var i = 0; i < dict.length; i++){
+        if(dict[i].date === date){
+          results[counter] = dict[i];
+          counter++;
+        }
+        else if (dict[i].period === period){
+          results[counter] = dict[i];
+          counter++;
+        }
+      }
     }
   });
 }

6dffb92a937732e6b039e4de64c3c028c1364a78
haftapparat <ahnesther0702@gmail.com>
2015-05-23 20:11:45 -0400

    pretty much finished the button input stuff

---
+++ survey/leavealone.js
@@ -0,0 +1,129 @@
+// Question 1 
+
+var bar = false;
+var clas = false;
+var rom = false;
+var imp = false;
+var twnc = false;
+var snd = false;
+var ane = false;
+
+
+function setBar() { 
+    bar = true;
+    clas = false;
+    rom = false;
+    imp = false;
+    twnc = false;
+    snd = false;
+    ane = false;
+}
+
+
+function setClas() {
+    bar = false;
+    clas = true;
+    rom = false;
+    imp = false;
+    twnc = false;
+    snd = false;
+    ane = false;
+}
+
+function setRom() {
+    bar = false;
+    clas = false;
+    rom = true;
+    imp = false;
+    twnc = false;
+    snd = false;
+    ane = false;
+}
+
+function setImp() {
+    bar = false;
+    clas = false;
+    rom = false;
+    imp = true;
+    twnc = false;
+    snd = false;
+    ane = false;    
+}
+
+function setTwnc() {
+    bar = false;
+    clas = false;
+    rom = false;
+    imp = false;
+    twnc = true;
+    snd = false;
+    ane = false;
+}
+
+function setSnd() {
+    bar = false;
+    clas = false;
+    rom = false;
+    imp = false;
+    twnc = false;
+    snd = true;
+    ane = false;
+}
+
+function setAne() {
+    bar = false;
+    clas = false;
+    rom = false;
+    imp = false;
+    twnc = false;
+    snd = false;
+    ane = true;
+}
+
+// Question 2
+
+var stormy = false;
+var sad = false;
+var joyful = false;
+var epic = false;
+var abstract = false;
+
+function setStormy() {
+    stormy = true;
+    sad = false;
+    joyful = false;
+    epic = false;
+    abstract = false;
+}
+
+function setSad() {
+    stormy = false;
+    sad = true;
+    joyful = false;
+    epic = false;
+    abstract = false;
+}
+
+function setJoyful() {
+    stormy = false;
+    sad = false;
+    joyful = true;
+    epic = false;
+    abstract = false;
+}
+
+function setEpic() {
+    stormy = false;
+    sad = false;
+    joyful = false;
+    epic = true;
+    abstract = false;
+}
+
+function setAbstract() {
+    stormy = false;
+    sad = true;
+    joyful = false;
+    epic = false;
+    abstract = false;
+}

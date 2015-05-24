ceca44866b19a42db3af31fee1c21247e596bd16
kushalk25 <kushal.k@hotmail.com>
2015-05-24 00:33:29 -0400

    added makeTable function

--- survey/music.js
+++ survey/music.js
@@ -437,6 +437,63 @@ for(var i = 0; i < results.length; i++){
 keyvalDict.sort(idArrayCompare);
 }
 
-//Now we can output results. Woo!!!!
+//Now we can output results.
+function makeTable(array) {
 
+    if(array.length == 0){
+        return;
+    }
 
+    // Create the list element:
+    var table = document.createElement('table');
+
+    var tableHead = document.createElement('thead');
+
+    var tHeadRow = document.createElement('tr');
+
+    var tHeadData = [];
+    for(var i = 0; i < 5; i++) {
+        tHeadData[i] = document.createElement('th');
+    }
+
+    tHeadData[0].appendChild(document.createTextNode("Piece"));
+    tHeadData[1].appendChild(document.createTextNode("Composer"));
+    tHeadData[2].appendChild(document.createTextNode("Era"));
+    tHeadData[3].appendChild(document.createTextNode("Mood"));
+    tHeadData[4].appendChild(document.createTextNode("Date Playing"));
+
+    for(var i = 0; i < 5; i++) {
+        tHeadRow.appendChild(tHeadData[i]);
+    }
+    tableHead.appendChild(tHeadRow);
+
+    var tableBody = document.createElement('tbody');
+
+    var tBodyRow = [];
+    var tBodyData = [];
+
+    for (var i = 0; i < array.length; i++) {
+        tBodyRow[i] = document.createElement('tr');
+
+        for (var j = 0; j < 5; j++) {
+            tBodyData[j] = document.createElement('td');
+        }
+        tBodyData[0].appendChild(document.createTextNode(array[i].name));
+        tBodyData[1].appendChild(document.createTextNode(array[i].composer));
+        tBodyData[2].appendChild(document.createTextNode(array[i].era));
+        tBodyData[3].appendChild(document.createTextNode(array[i].mood));
+        tBodyData[4].appendChild(document.createTextNode(array[i].date));
+
+        for(var k = 0; k < 5; k++) {
+            tBodyRow[i].appendChild(tBodyData[k]);
+        }
+
+        tableBody.appendChild(tBodyRow[i]);
+    }
+
+    table.appendChild(tableHead);
+    table.appendChild(tableBody);
+
+    // Finally, return the constructed table:
+    return table;
+}
